b9205440c29ebfe0f0d1baaa6fe48a8f1821676d
Luiz Guilherme <dr.ak13021@gmail.com>
2023-09-21 18:31:20 -0300

    Moving Pieces

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Chessmatch;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Chessmatch chessmatch = new Chessmatch();
 
-        UI.printBoard(chessmatch.getPieces());
+        while (true) {
+            UI.printBoard(chessmatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturePiece = chessmatch.performChessMove(source, target);
+        }
+
     }   
 }
