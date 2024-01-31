c7d04a933aa14fa1f04c012f48c1d1a173a2e84d
Luiz Guilherme <dr.ak13021@gmail.com>
2024-01-31 00:12:22 -0300

    Check logic

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -12,7 +12,7 @@ public class King extends ChessPiece {
 
     @Override
     public String toString(){
-        return "k";
+        return "K";
     }
 
     private boolean canMove(Position position){
